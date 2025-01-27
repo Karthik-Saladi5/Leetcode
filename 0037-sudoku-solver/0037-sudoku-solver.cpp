719d7e52b10a549e1aa562505a5ed93e2a9785a0
Saladi Sri Sai Karthik <119729884+Karthik-Saladi5@users.noreply.github.com>
2025-01-27 22:47:34 +0530

    Time: 426 ms (34.26%), Space: 8.9 MB (29.86%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    bool isValid(vector<vector<char>> &board, int row,int col,char ch){
+        for(int i=0;i<9;i++){
+            if(board[i][col]==ch) return 0;
+            if(board[row][i]==ch) return 0;
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3] == ch) return 0;
+        }
+        return 1;
+    }
+    bool rec(vector<vector<char>> &board){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.'){
+                    for(char ch='1';ch<='9';ch++){
+                        if(isValid(board,i,j,ch)){
+                            board[i][j]=ch;
+                            if(rec(board)) return 1;
+                            board[i][j]='.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return 1;
+    }
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        rec(board);
+    }
+};
