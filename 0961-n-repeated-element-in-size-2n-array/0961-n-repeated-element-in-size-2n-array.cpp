afcab2cb436efb6dd6af6d0b2801ce10ecc60b75
Saladi Sri Sai Karthik <119729884+Karthik-Saladi5@users.noreply.github.com>
2026-01-02 23:10:21 +0530

    Time: 0 ms (100%), Space: 28.4 MB (69.74%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_map<int,int> freq;
+        for(auto i:nums){
+            freq[i]++;
+            if(freq[i]==2) return i;
+        }
+        return 0;
+    }
+};
