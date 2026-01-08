a8c9894c91fcd3361f54abf7229164f3e3a66f01
Saladi Sri Sai Karthik <119729884+Karthik-Saladi5@users.noreply.github.com>
2026-01-08 11:41:12 +0530

    Time: 1 ms (11.47%), Space: 10.6 MB (84.48%) - LeetHub

---
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        int i=0;
+        vector<string> ans;
+        for(int j=1;j<=n;j++){
+            if(i>=target.size()) break;
+            if(target[i]==j){
+                ans.push_back("Push");
+                i++;
+            }else{
+                ans.push_back("Push");
+                ans.push_back("Pop");
+            }
+        }
+        return ans;
+    }
+};
