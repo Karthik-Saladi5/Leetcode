0572f2e231ac82b33f4d6896706e869c05be4a0a
Saladi Sri Sai Karthik <119729884+Karthik-Saladi5@users.noreply.github.com>
2026-02-06 23:44:48 +0530

    Time: 32 ms (59.88%), Space: 105 MB (52.91%) - LeetHub

---
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int i = 0;
+        int ans = 0;
+        for (int j = 0; j < n; j++) {
+            while ((long long)nums[j] > (long long)nums[i] * k)
+                i++;
+            ans = max(ans, j - i + 1);
+        }
+
+        return n - ans;
+    }
+};
